04f4ae14b36af002778259fc0604900491092e61
AmirHossein <ah.n2002@yahoo.com>
2022-01-31 21:11:08 +0330

    start...

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <SDL.h>
+#include <SDL2_gfxPrimitives.h>
+#ifdef main
+#undef main
+#endif
+//#################################################################################################
+#include <stdio.h>
+
+int main() {
+    
+    
+    return 0;
+}
